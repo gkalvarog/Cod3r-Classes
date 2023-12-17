16227f085f3d7f12645908f2bcc8413b7417e461
gkalvarog <g.kalvaro.ag@gmail.com>
2023-12-17 13:32:34 +0300

    first commit

---
+++ cod3r_beginner.py
@@ -0,0 +1,23 @@
+# standard way to create a dictionary
+dicionario = {}
+
+# create a dict using prototype
+my_dict = dict()
+
+# examples
+my_new_dict = {'nome': 'Abc', 'idade':26, 'altura':1.77}
+
+print(my_new_dict)
+print(my_new_dict['idade'])
+
+# methods for dictionaries
+my_new_dict['programmer'] = True
+print(my_new_dict)
+
+# overwrite items
+my_new_dict['altura'] = 245
+print(my_new_dict)
+
+# loops for dictionaries
+for i in my_new_dict:
+    print(i, my_new_dict[i])
