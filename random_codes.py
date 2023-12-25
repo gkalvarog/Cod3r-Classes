367003d563df04d2508e65f894cbcb4812abec3e
Alvin <103211949+gkalvarog@users.noreply.github.com>
2023-12-25 14:15:14 +0300

    add. random codes (studies)

---
+++ random_codes.py
@@ -0,0 +1,8 @@
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+travel_log["France"] = {"cities_visited": ["Paris", "Lille", "Dijon"]}
+
+print(travel_log)
